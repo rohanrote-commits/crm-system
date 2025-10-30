517876791b71b0a503a9bbbaac4341d83121d5f4
Rohan Rote <rohan.rote@perennialsys.com>
2025-10-30 15:35:43 +0530

    Created Project Setup for Backend

---
+++ crm-system-backend/src/main/java/com/example/crm_system_backend/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.crm_system_backend.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/crm/")
+public class UserController {
+}
