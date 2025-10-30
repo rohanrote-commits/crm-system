60974c8a4a2794cbd7bf5a277852d2b66766be21
Rohan Rote <rohan.rote@perennialsys.com>
2025-10-30 15:22:18 +0530

    first commit

---
+++ crm-system-backend/src/main/java/com/example/crm_system_backend/CrmSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.crm_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmSystemBackendApplication.class, args);
+	}
+
+}
